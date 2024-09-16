f9dee16d1106b5054d422b4c8dde91fb7da1e6e9
BigBearGaz <toto.rez.21@gmail.com>
2024-09-16 19:44:18 +0200

    hth

---
+++ admin/body/topbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar bg-primary mb-5">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">BlogDeGazo</a>
+    <a class="nav-link active" aria-current="page" href="index.php?page=dashboard"><i class="material-icons">dashboard</i></a>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+                    <a class="nav-link active" aria-current="page" href="index.php?page=dashboard"><i class="material-icons">dashboard</i></a>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php?page=dashboard"><i class="material-icons">dashboard</i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?page=dashboard"><i class="material-icons">dashboard</i></a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
---
+++ admin/pages/dashboard.php
@@ -0,0 +1,12 @@
+
+<div class="row">
+    <div class="col l4 m6 s12 offest-l4 offset-m3">
+        <div class="card-pannel">
+            <div class="row">
+                <div class="col s6 offset-s3">
+                    <img src="../img/admin.png" alt="Administrateur"width="100px"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>m3
